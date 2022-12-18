2aa3a6ff214f2b55ffd0c9793e44e0fe9d0e3f9d
Alina <alinakonculizova@gmail.com>
2022-12-18 21:28:23 +0300

    примитивные типы

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("Hello World");
+const salaryAtHour = 80;
+const noMoreHours = 5;
+const noMoreDays = 5;
+
+const orderHours = 40;
+const currentDay = 1;
+
+const xDay = 12;
+
+const workDays = xDay  - (noMoreDays - currentDay);
+const totalWorkHours = workDays * noMoreHours;
+
+const myMoney = workDays * salaryAtHour * noMoreHours;
+const isWellGetWork = totalWorkHours <= orderHours;
+
+console.log('хочу столько денег за работу: ', myMoney + '$');
+console.log('хватит дней на работу? ', isWellGetWork);
