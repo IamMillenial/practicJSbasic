b55195a3c866b00d5d4445a865cc24b7502cbea0
Alina <alinakonculizova@gmail.com>
2022-12-23 18:30:19 +0300

    итерация в массивах

--- app.js
+++ app.js
@@ -1,45 +1,8 @@
+const prices =[[100, 200], [120, 100], [200, 350]];
 
-const operations = [1000, -700, 300, -500, 10000];
-const startingBalance = 100;
-function getBalance(arrayofoperations, initaiBalance) {
-    let balance = initaiBalance;
-    for (const element of arrayOfOperations) {
-        balance += element;
-    }
-    return balance;
-}
- console.log(getBalance(operations, startingBalance))
+const positivePrices = prices.filter(price => {
+        return (price[1]-price[0]) > 0
+    })
+    .map(positivePrices => positivePrices[1] - positivePrices[0])
 
-function checkOperations(arrayofoperations, initaiBalance) {
-    let balance = initaiBalance;
-    let isOk = true;
-    for (const element of arrayOfOperations) {
-        balance += element;
-        if (balance<0) {
-            isOk = false;
-            break;
-        }
-    }
-    return isOk;
-}
-console.log(checkOperations(operations, startingBalance))
-
-function avarageOperetions(arrayofoperations) {
-    let positiveCount = 0;
-    let positiveSum = 0;
-    let negativeCount = 0;
-    let negativeSum = 0;
-        for (const element of arrayOfOperations) {
-            if (element > 0) {
-                positiveCount++;
-                positiveSum += element;
-            }
-            if (element < 0) {
-                negativeCount++;
-                negativeSum += element;
-            }
-        }
-            return [positiveSum/positiveCount, negativeSum/negativeCount]
-        }
-
-    console.log(avarageOperetions(operations));
+console.log(positivePrices);
