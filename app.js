0e485a9c8f7fd8c3f6ae31a6dafea7f158a103f5
Alina <alinakonculizova@gmail.com>
2022-12-25 17:43:19 +0300

    строки 2 наш варик

--- app.js
+++ app.js
@@ -1,8 +1,44 @@
-const username = 'Вася aka Terminator Perdinator Пупкин';
-const arr = username.split(' ');
-const firstName =arr[0];
-const lastName =arr[4];
-console.log(arr);
+const num1 = '89103235356';
+const num2 = '+79103235356';
+const num3 = '+7(910)3235356';
+const num4 = '+7(910) 323-53-56';
+const num5 = ' +7(910) 323-53-56 ';
 
-console.log(firstName);
-console.log(lastName);
+const num1Error = '89103235';
+const num2Error = '+7d910d323-53-56';
+const num3Error = '9+7103235356';
+const num4Error = '89103g35356';
+
+const isRussianNumber = (str) => {
+const clearString = str.replaceAll('-', ' ')
+    .replaceAll('(', ' ')
+    .replaceAll(')', ' ')
+    .replaceAll(' ', '')
+    .trim();
+
+let numstring = '';
+
+    for (let i = 0; i <= clearString.length - 1; i += 1) {
+        if((i === 0 && clearString[i] === '+') || !isNaN(Number(clearString[i]))) {
+            numstring += clearString[i]
+        }
+    }
+
+    console.log('numstring', numstring)
+
+
+
+   return (numstring.length === 11 || numstring.length === 12)
+    && (numstring.startsWith('+7') || numstring.startsWith('8'))
+}
+
+console.log(isRussianNumber(num1));
+console.log(isRussianNumber(num2));
+console.log(isRussianNumber(num3));
+console.log(isRussianNumber(num4));
+console.log(isRussianNumber(num5));
+
+console.log(isRussianNumber(num1Error));
+console.log(isRussianNumber(num2Error));
+console.log(isRussianNumber(num3Error));
+console.log(isRussianNumber(num4Error));
