73670a8cfc6e6fd13fc162776b0afa7ab5207ebb
Alina <alinakonculizova@gmail.com>
2022-12-27 21:36:08 +0300

    объект в объекте

--- app.js
+++ app.js
@@ -1,75 +1,31 @@
-const warehouse ={
-    goods: [],
-    findGoodbyId: function (id) {
-        return this.goods.find(g => g.id == id);
-    },
-
-
-    //     let users = [
-    //     {id: 1, name: "Вася"},
-    //     {id: 2, name: "Петя"},
-    //     {id: 3, name: "Маша"}
-    // ];
-    //
-    // let user = users.find(item => item.id == 1);
-
-    addGood: function (good) {
-        const existedGood = this.findGoodbyId(good.id);
-        if (existedGood) {
-            console.log('Этот товар уже есть на складе');
-            return;
+const company = {
+    name: 'OOO AGRO',
+    employees: [
+        {
+            name: 'Sveta',
+            getName: function () {
+                return this.name;
+            }
+        },
+        {
+            name: 'Sasha',
+            getName: function () {
+                return this.name;
+            }
         }
-        this.goods.push(good);
-    },
-
-    // let arr = [1, 2, 3, 4, 5];
-    //
-    // let result = arr.reduce((sum, current) => sum + current, 0);
-    //
-    // alert(result); // 15
-
-    getWeightKg: function () {
-        return this.goods.reduce((acc, el) =>
-            acc += el.weight?.kg ? el.weight.kg : 0,
-        0
-        )
-    },
-};
-
-/*
-    getWeightKg: function () {
-        return this.goods.reduce((acc, el) => {
-
-        if(el.weight !== 'undefined' && el.weight.kg !== 'undefined') {
-            return acc += el.weight.kg
-        } else {
-            return acc += 0
+    ],
+    ceo: {
+        name: 'Vasya',
+        getName: function () {
+            return this.name;
         }
+    },
 
-
-        }, 0)
+    getName: function () {
+        return this.name;
     }
-*/
-
-const car = {
-    id: 1,
-    weight: {
-        kg: 1000
-    },
-    brand: 'Ford'
-};
-const chair = {
-    id: 1,
-    weight: {
-        kg:2
-    },
-};
-const paper = {
-    id: 1,
-    color: 'red'
 }
+console.log(company.getName());
+console.log(company.ceo.getName());
+console.log(company.employees.map(employee => employee.getName()));
 
-warehouse.addGood(car);
-warehouse.addGood(chair);
-warehouse.addGood(paper);
-console.log(warehouse.goods);
